77a7372e7f276967d9786b238926098cec334a71
Morgan Burcham <ke5vfk@gmail.com>
2022-05-08 05:53:33 +0000

    Wrapping map creation in function

--- m.py
+++ m.py
@@ -1,17 +1,55 @@
 import streamlit as st
 import pandas as pd
 
+def app(df): 
+    with st.spinner("Preparing map..."):
+        # st.pydeck_chart(pdk.Deck(
+        # map_style='mapbox://styles/mapbox/light-v9',
+        # initial_view_state=pdk.ViewState(
+        #     latitude=37.76,
+        #     longitude=-122.4,
+        #     zoom=11,
+        #     pitch=50,
+        # ),
+        # layers=[
+        #     pdk.Layer(
+        #         'HexagonLayer',
+        #         data=df,
+        #         get_position='[lon, lat]',
+        #         radius=200,
+        #         elevation_scale=4,
+        #         elevation_range=[0, 1000],
+        #         pickable=True,
+        #         extruded=True,
+        #     ),
+        #     pdk.Layer(
+        #         'ScatterplotLayer',
+        #         data=df,
+        #         get_position='[lon, lat]',
+        #         get_color='[200, 30, 0, 160]',
+        #         get_radius=200,
+        #     ),
+        # ],
+        # ))
+        st.map(df)
 
-us_data = pd.read_parquet('final_data.parquet')
-lats = us_data['decimalLatitude']
-longs = us_data['decimalLongitude']
-mask = longs.isna() | lats.isna()
+    #TODO Filter out non-US (maybe)
 
-df = pd.DataFrame()
-df['lat'] = lats
-df['lon'] = longs
 
-df = df[~mask]
-st.map(df, zoom=9)
+df = None
 
-#TODO Filter out non-US (maybe)
+with st.spinner("Preparing data..."):
+    us_data = pd.read_parquet('final_data.parquet')
+    lats = us_data['decimalLatitude']
+    longs = us_data['decimalLongitude']
+    mask = longs.isna() | lats.isna()
+
+    df = pd.DataFrame()
+    del us_data
+    df['lat'] = lats
+    df['lon'] = longs
+
+    df = df[~mask]
+
+app(df)
+
